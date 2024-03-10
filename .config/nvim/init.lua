fd454e29b4fed507f35498fe7903ab770a087eb5
Alexander Kalinin <alex@kalinin.uk>
2024-03-10 23:44:05 +0800

    tmux navigator

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -234,6 +234,31 @@ require("lazy").setup({
 	--  This is equivalent to:
 	--    require('Comment').setup({})
 
+	{
+		"alexghergh/nvim-tmux-navigation",
+		config = function()
+			local nvim_tmux_nav = require("nvim-tmux-navigation")
+
+			nvim_tmux_nav.setup({
+				disable_when_zoomed = true, -- defaults to false
+			})
+
+			vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+			vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+			vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+			vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+			vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+			vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+		end,
+	},
+
+	"wuelnerdotexe/vim-astro",
+
+	"tpope/vim-fugitive",
+	"tpope/vim-rhubarb",
+	"tpope/vim-surround",
+	"tpope/vim-repeat",
+
 	-- "gc" to comment visual regions/lines
 	{ "numToStr/Comment.nvim", opts = {} },
 
