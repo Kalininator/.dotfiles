5abb89d68cc930764286490c6874fedff4904673
Alexander Kalinin <alex@kalinin.uk>
2025-12-15 19:12:55 +0000

    claude

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -295,6 +295,17 @@ require('lazy').setup({
   'tpope/vim-repeat',
   'tpope/vim-rails',
 
+  {
+    'greggh/claude-code.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- Required for git operations
+    },
+    config = function()
+      require('claude-code').setup()
+      vim.keymap.set('n', '<leader>cc', '<cmd>ClaudeCode<CR>', { desc = 'Toggle Claude Code' })
+    end,
+  },
+
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
